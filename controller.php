f951a679a58ca36ed8e011830f7b1644d55f18da
asbarros <anderson.barros0298@gmail.com>
2019-07-16 12:58:55 -0300

    melhorias

--- controller.php
+++ controller.php
@@ -1,4 +1,15 @@
 <?php
 
+if (isset($_POST[['Enviar']])) {
+    $formatosPermitidos = array('png', 'jpeg', 'jpg', 'gif');
+    $extencao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
+    if (in_array($extencao, $formatosPermitidos)) {
+        $pasta = 'arquivos/';
+        $temporario = $_FILES['arquivo']['tmp_name'];
+        $novoNome = uniqid().$extencao;
+    } else {
+        $mensagem[] = 'Formato inválido!';
+    }
+}
 $files = $_FILES['files'];
-echo move_uploaded_file($files['tmp_name'], "/home/anderson/$files[name]");
+ move_uploaded_file($files['tmp_name'], "/home/anderson/$files[name]");
